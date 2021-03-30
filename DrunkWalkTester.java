315da4652ed5a70ddfc9e945086d59b7a4cf6a95
Daniel Targonski <daniel.targonski2@gmail.com>
2021-03-29 21:46:01 -0400

    complete

--- DrunkWalkTester.java
+++ DrunkWalkTester.java
@@ -3,70 +3,79 @@ package edu.cuny.csi.csc330.lab5;
 import java.util.Scanner;
 
 public class DrunkWalkTester {
-	
+
 	private Scanner input;
 
 	public DrunkWalkTester() {
-		init();  
+		init();
 	}
-	
+
 	private void init() {
 		input = new Scanner(System.in);
 	}
-	
-	public void runTest(int steps ) { 
-		
-		
+
+	public void runTest(int steps) {
+
 		System.out.print("Enter the starting avenue value: ");
 		int avenue = input.nextInt();
 		System.out.print("Enter the starting street value: ");
 		int street = input.nextInt();
-		
-		//////////////////////// start test 
-		// make the Drunkard with initial position
-		DrunkWalker billy = new DrunkWalker(avenue,street);
 
-		// have him move/step 200 time 
+		//////////////////////// start test
+		// make the Drunkard with initial position
+		DrunkWalker billy = new DrunkWalker(avenue, street);
+		DrunkWalker harvey = new DrunkWalker(avenue, street);
+
+		// have him move/step 200 time
 		billy.fastForward(steps);
-		
+		harvey.fastForward(steps);
+
 		// get his current location
-		String location = billy.getLocation();
-		
+		String locationBilly = billy.getLocation();
+		String locationHarvey = harvey.getLocation();
 		// get distance from start
 		int distance = billy.howFar();
+		int distance2 = harvey.howFar();
 
-		System.out.println("Billy's " + location);
+		System.out.println("Billy's " + locationBilly);
 		System.out.println("That's " + distance + " blocks from start.");
-		
+
+		System.out.println("Harvey's " + locationBilly);
+		System.out.println("That's " + distance2 + " blocks from start.");
+
+		if (distance > distance2) {
+			System.out.println("Billy walked a greater distance with " + distance + " blocks from the start.");
+		} else
+			System.out.println("Harvey walked a greater distance with " + distance2 + " blocks from the start.");
+
 		billy.displayWalkDetails();
-		
-		//////////////////// end test 
-		/** 
-		 * Expand the test above to include the following ... 
-		 * Create a 2nd instances of DrunkWalker - Harvey  
-		 * Have then race each - which instance (billy or harvey)  
-		 * manages to walk a greater distance with 200 steps?  
+		harvey.displayWalkDetails();
+
+		//////////////////// end test
+		/**
+		 * Expand the test above to include the following ... Create a 2nd instances of
+		 * DrunkWalker - Harvey Have then race each - which instance (billy or harvey)
+		 * manages to walk a greater distance with 200 steps?
 		 * 
 		 * Also invoke the displayWalkDetails() on both instances.
- */
-
+		 */
 
 	}
 
 	/**
-	 * @param args 
+	 * @param args
 	 * 
 	 */
 	public static void main(String[] args) {
 		DrunkWalkTester tester = new DrunkWalkTester();
-		
-		int steps = 2000; 
-		if(args.length == 1) {
+
+		int steps = 200;
+		if (args.length == 1) {
 			steps = Integer.parseInt(args[0]);
 		}
-		
-		tester.runTest(steps); 
-		
+
+		tester.runTest(steps);
+
 		System.exit(0);
 
 	}
--- DrunkWalker.java
+++ DrunkWalker.java
@@ -1,175 +1,224 @@
 package edu.cuny.csi.csc330.lab5;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class DrunkWalker {
-	
+
 	private Intersection startIntersection;
 	private Intersection currentIntersection;
+	List<Intersection> journey;
+	Map<Intersection, Integer> intersectionCount;
+	private Integer stepCounter;
 
-	
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
-	// add other data members here including Collection instances that you will use to 
-	//  to track history and statistics ... 
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// add other data members here including Collection instances that you will use
+	// to
+	// to track history and statistics ...
 
 	private DrunkWalker() {
 		init();
 	}
-	
+
 	/**
 	 * 
 	 * @param avenue
 	 * @param street
 	 */
-	public DrunkWalker(int avenue, int street) {	
+	public DrunkWalker(int avenue, int street) {
 		init();
 		startIntersection = new Intersection(avenue, street);
 		currentIntersection = new Intersection(avenue, street);
 	}
-	
+
 	private void init() {
 		// What should we do here to initialize an instance??
-		// Create the Collection that tracks the journey 1 to N Intersections 
-
-        // Create the Collection allows me to map Intersections to counts - number of times we land there 
-
-        // Create other Collections instances that would facilitate easier management of what needs to be tracked 
+		// Create the Collection that tracks the journey 1 to N Intersections
+		journey = new ArrayList<Intersection>();
+		// Create the Collection allows me to map Intersections to counts - number of
+		// times we land there
+		intersectionCount = new TreeMap<Intersection, Integer>();
+		// Create other Collections instances that would facilitate easier management of
+		// what needs to be tracked
+		stepCounter = 1;
 
 	}
-	
+
 	/**
-	 * step in a random direction 
+	 * step in a random direction
 	 */
 	public void step() {
-		
-		takeAStep(); 
-		
-		/**  !!!!!!!!!!!!!!!!!!!!!!!!!!!
-		 * Now, update the Collections that manage the following:
+
+		takeAStep();
+		journey.add(currentIntersection);
+
+		/*
+		 * If intersection count containsKey of currentIntersection that means we were
+		 * at this intersection before. So we increment the value of the key. else, its
+		 * the first time we are at the currentIntersection, so we .put the K,V pair of
+		 * the currentIntersection and value = 0
+		 */
+		if (intersectionCount.containsKey(currentIntersection)) {
+			intersectionCount.put(currentIntersection, stepCounter + 1);
+		} else {
+			intersectionCount.put(currentIntersection, stepCounter);
+		}
+
+		/**
+		 * !!!!!!!!!!!!!!!!!!!!!!!!!!! Now, update the Collections that manage the
+		 * following:
 		 * 
-		 *  1) add this next step/intersection to a "history" List that will contain the sequence of all 
-		 *  steps taken by this DrunkWalker instance 
-		 *  
-		 *  2) add this next step to a Intersection -> Counter Map ... The Map 
-		 *  Collection can and should be of Generics "Type" <Intersection, Integer> 
-		 *  key = Intersection 
-		 *  value = Count Value  
-		 *  Need to do something like this: 
-		 *  if intersection is not saved in Map yet as a key yet, add a key->value pair of Intersection->1 
-		 *  else if intersection value is there, the existing key->value pair need to be replaced as 
-		 *   Intersection->existing_count+1 
-		 *   
+		 * 1) add this next step/intersection to a "history" List that will contain the
+		 * sequence of all steps taken by this DrunkWalker instance
+		 * 
+		 * 2) add this next step to a Intersection -> Counter Map ... The Map Collection
+		 * can and should be of Generics "Type" <Intersection, Integer> key =
+		 * Intersection value = Count Value Need to do something like this: if
+		 * intersection is not saved in Map yet as a key yet, add a key->value pair of
+		 * Intersection->1 else if intersection value is there, the existing key->value
+		 * pair need to be replaced as Intersection->existing_count+1
+		 * 
 		 */
 	}
-	
-	
+
 	private void takeAStep() {
-		Direction dir = Direction.NONE.getNextRandom(); 
-		
-		/** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		 * now what do we do based on the random Direction created? 
-		 * How do we go about updating the "currentIntersection" instance to reflect the 
-		 * direction/step that was just selected? 
+		Direction dir = Direction.NONE.getNextRandom();
+
+		Intersection nextIntersection = new Intersection(currentIntersection.getAvenue(),
+				currentIntersection.getStreet());
+
+		if (dir == Direction.SOUTH) { // south
+			nextIntersection.setStreet(nextIntersection.getStreet() - 1);
+		} else if (dir == Direction.WEST) { // west
+			nextIntersection.setAvenue(nextIntersection.getAvenue() + 1);
+		} else if (dir == Direction.NORTH) { // north
+			nextIntersection.setStreet(nextIntersection.getStreet() + 1);
+		} else if (dir == Direction.EAST) { // east
+			nextIntersection.setAvenue(nextIntersection.getAvenue() - 1);
+		} else if (dir == Direction.NORTHEAST) { // northeast
+			nextIntersection.setAvenue(nextIntersection.getAvenue() - 1);
+			nextIntersection.setStreet(nextIntersection.getStreet() + 1);
+		} else if (dir == Direction.NORTHWEST) { // northwest
+			nextIntersection.setAvenue(nextIntersection.getAvenue() + 1);
+			nextIntersection.setStreet(nextIntersection.getStreet() + 1);
+		} else if (dir == Direction.SOUTHWEST) { // south-west
+			nextIntersection.setAvenue(nextIntersection.getAvenue() + 1);
+			nextIntersection.setStreet(nextIntersection.getStreet() - 1);
+		} else { // south-east
+			nextIntersection.setAvenue(nextIntersection.getAvenue() - 1);
+			nextIntersection.setStreet(nextIntersection.getStreet() - 1);
+		}
+
+		currentIntersection = nextIntersection;
+
+		/**
+		 * !!!!!!!!!!!!!!!!!!!!!!!!!!!!! now what do we do based on the random Direction
+		 * created? How do we go about updating the "currentIntersection" instance to
+		 * reflect the direction/step that was just selected?
 		 */
-		
-		
+
 	}
 
-
-	/** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	 * toString() 
+	/**
+	 * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! toString()
+	 * 
 	 * @return
 	 */
-	
-
-
+	@Override
+	public String toString() {
+		return "DrunkWalker [startIntersection=" + startIntersection + ", currentIntersection=" + currentIntersection
+				+ ", journey=" + journey + ", intersectionCount=" + intersectionCount + "]";
+	}
 
 	/**
-	 * generate string that contains current intersection/location info 
+	 * generate string that contains current intersection/location info
 	 */
 	public String getLocation() {
-		// !!!!!!!!!!!!!!!!!  
-		/**
-		return String.format("Current location: DrunkWalker [avenue=%d, street=%d]", 
-				currentIntersection.getAvenue(), currentIntersection.getStreet() );
-		*/
-		return  null;  
+		return String.format("Current location: DrunkWalker [avenue=%d, street=%d]", currentIntersection.getAvenue(),
+				currentIntersection.getStreet());
 	}
 
 	/**
-	 * Take N number of steps 
+	 * Take N number of steps
+	 * 
 	 * @param steps
 	 */
-	public void fastForward(int steps ) {
-		// Considering that we already have a step() method, how would we 
-		//  implement this method? think reuse!  
+	public void fastForward(int steps) {
+		// Considering that we already have a step() method, how would we
+		// implement this method? think reuse!
+		for (int i = 0; i < steps; i++) {
+			step();
+		}
 	}
-	
+
 	/**
-	 * Display information about this current walker instance 
+	 * Display information about this current walker instance
 	 */
 	public void displayWalkDetails() {
 		/**
-		 * This method needs to display the following information in a neat, readable format 
-		 * using calls to System.out.println() or System.out.printf()
+		 * This method needs to display the following information in a neat, readable
+		 * format using calls to System.out.println() or System.out.printf()
 		 * 
-		 * 1 - starting location 
-		 * 2 - current/ending location 
-		 * 3 - distance (value returned by howFar() ) 
-		 * 4 - number of steps taken - which collection would be able to provide that information, and how?  
-		 * 5 - number of unique intersections visited - 
-		 * 		which collection would be able to provide that information, and how? 
-		 * 6 - Intersections visited more than once 
+		 * 1 - starting location 2 - current/ending location 3 - distance (value
+		 * returned by howFar() ) 4 - number of steps taken - which collection would be
+		 * able to provide that information, and how? 5 - number of unique intersections
+		 * visited - which collection would be able to provide that information, and
+		 * how? 6 - Intersections visited more than once
 		 * 
 		 */
-		
-		
-		
-		
-		
+
+		System.out.printf("%nStarting Location: Intersection [avenue=%d, street=%d]%n", startIntersection.getAvenue(),
+				startIntersection.getStreet());
+
+		System.out.printf("Current/Ending Location: Intersection [avenue=%d, street=%d]%n",
+				currentIntersection.getAvenue(), currentIntersection.getStreet());
+
+		System.out.printf("Distance (blocks): %d %n", howFar());
+		System.out.printf("Number of steps taken: %d %n", journey.size());
+		System.out.printf("Number of unique intersections visited: %d %n%n", intersectionCount.size());
+
+		intersectionCount.forEach((k, v) -> System.out.printf("Visited Intersection [avenue=%d, street=%d] %d times!%n",
+				k.getAvenue(), k.getStreet(), v));
+
 	}
-	
+
 	/**
-	 * X Y Coordinate distance formula 
-	 *  |x1 - x2| + |y1 - y2|   
+	 * X Y Coordinate distance formula |x1 - x2| + |y1 - y2|
+	 * 
 	 * @return
 	 */
 	public int howFar() {
-		/** |x1 - x2| + |y1 - y2|.
-		startAvenue = x1 
-		avenue = x2 
-		startStreet = y1 
-		street = y2 
-	 
-		return (Math.abs(startAvenue - avenue) ) + (Math.abs(startStreet - street)); 
-		 * 
-		 */
-		
-		return 0; 
+		// |x1 - x2| + |y1 - y2|.
+		int startAvenue = startIntersection.getAvenue();
+		int avenue = currentIntersection.getAvenue();
+		int startStreet = startIntersection.getStreet();
+		int street = currentIntersection.getStreet();
+
+		return (Math.abs(startAvenue - avenue)) + (Math.abs(startStreet - street));
 	}
 
-
 	public static void main(String[] args) {
-		
+
 		// create Drunkard with initial position (ave,str)
-		DrunkWalker billy = new DrunkWalker(6,23);
-		
-		for(int i = 1 ; i <= 3 ; ++i ) {
-			billy.step(); 
-			System.out.printf("billy's location after %d steps: %s\n",
-					i, billy.getLocation() );
+		DrunkWalker billy = new DrunkWalker(6, 23);
+
+		for (int i = 1; i <= 3; ++i) {
+			billy.step();
+			System.out.printf("billy's location after %d steps: %s\n", i, billy.getLocation());
 		}
-			
-		
+
 		// get his current location
 		String location = billy.getLocation();
 		// get distance from start
 		int distance = billy.howFar();
+
 		System.out.println("Current location after fastForward(): " + location);
 		System.out.println("That's " + distance + " blocks from start.");
-		
 
-		// have him move 25  random intersections
+		// have him move 25 random intersections
 		billy.fastForward(25);
 		billy.displayWalkDetails();
 
